6f73badbf692243daecbcdb249e7ed96ab700207
TatianaShuvaeva <talisman1982@gmail.com>
2025-02-05 16:56:31 +0000

    employeers APP

--- src/App.js
+++ src/App.js
@@ -1,58 +1,10 @@
 import { Component } from 'react';
 import './App.css';
 
-
-const Header = () => {
-  return <h2>Hello World !</h2>
-} 
-
-// const Field = () => {
-//   const holder = 'Enter here'
-//   const styledField = {
-//     with: '300px'
-//   }
-//   return <input 
-//           placeholder= {holder}
-//           type ='text'
-//           style={styledField}/>
-
-// }
-
-class Field extends Component {
-  render() {
-    const holder = 'Enter here'
-    const styledField = {
-      width: '300px'
-    }
-  
-  return<input 
-          placeholder = { holder }
-          type = 'text'
-          style = { styledField }/>
-  };
-}
-
-function Btn () {
-  const text = 'Log in';
-  // const p = <p>Log in</p>
-
-  // const res = () => {
-  //   return 'Log in';
-  // }
-
-  const logged = true;
-
-  // return <button>{res()}</button>
-  // return <button>{p}</button>
-  return <button>{logged ? 'Enter': text}</button>
-}
-
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Field/>
-      <Btn/>
+    
     </div> 
   );
 }
